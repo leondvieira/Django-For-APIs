8a3cf352973b538f573cc5a01284a05b73aacf36
Leonardo Vieira <leonardovieirabfh@gmail.com>
2021-01-29 18:11:04 -0300

    Chapter 2 finished

---
+++ library/books/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path
+
+from .views import BookListView
+
+app_name = 'books'
+
+urlpatterns = [
+    path('', BookListView.as_view(), name='home'),
+]
